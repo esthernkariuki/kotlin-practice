426c11307c60dcbd759effc294e105c12b2c3fdf
esther <esthernnyamburaa@gmail.com>
2025-03-27 14:12:43 +0300

    variables

---
+++ variables.kt
@@ -0,0 +1,12 @@
+fun main (){  var name ="Jane"
+    name = "Mary"
+
+ var age:Byte = 20
+    var students:Short = 5000
+    var balance:Int = 8789021
+    var kenyan = true
+    var x = "t"
+    var y = "School"
+    var fruits = arrayOf("girl","school",true,54,2025)
+
+        }
